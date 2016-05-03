b467848b3ac8af345d0b6e9d32f38538881b99fb
Jie Lin <89507.lin@students.itu.edu>
2016-05-03 04:32:27 +0000

    Question 2

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,3 +1,11 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
+  def initialize(k) String
+      @bar = k
+  end
+  
+  def bar (var1, options={})
+      return var1.to_s + @bar + options[:sat].to_s
+  end
+
 end
